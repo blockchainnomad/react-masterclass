2eb599cb984b5a9680cd02743b238caae4cb49ee
blockchainnomad <blockchainnomad@gmail.com>
2023-06-27 22:33:03 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,27 @@
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: tomato;
+  &:hover {
+    color: teal;
+  }
+`;
+
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotateAnimation = keyframes`
-0% {
-  transfrom: rotate(0deg);
-  border-radius: 0px;
-}
-50% {
-  transform: rotate(360deg);
-  border-radius: 100px;
-}
-100%{
-  transform: rotate(0deg);
-  border-radius: 0px;
-}
-`;
-
-const Emoji = styled.div`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  animation: ${rotateAnimation} 1s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 98px;
-    }
+  ${Title}:hover {
+    font-size: 99px;
   }
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>🤩</Emoji>
-      </Box>
-      <Emoji>🐸</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
